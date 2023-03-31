a9e574ee31ed67d4422e4926a6a1e463054a651f
Rafael Santos <rafaelcs_94@hotmail.com>
2023-03-31 12:05:22 -0300

    Project finished

--- src/main/java/com/trybe/conversorcsv/Conversor.java
+++ src/main/java/com/trybe/conversorcsv/Conversor.java
@@ -1,7 +1,12 @@
 package com.trybe.conversorcsv;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Conversor {
 
@@ -9,8 +14,8 @@ public class Conversor {
    * Função utilizada apenas para validação da solução do desafio.
    *
    * @param args Não utilizado.
-   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou
-   *                     gravar os arquivos de saída.
+   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou gravar os
+   *         arquivos de saída.
    */
   public static void main(String[] args) throws IOException {
     File pastaDeEntradas = new File("./entradas/");
@@ -19,17 +24,87 @@ public class Conversor {
   }
 
   /**
-   * Converte todos os arquivos CSV da pasta de entradas. Os resultados são gerados
-   * na pasta de saídas, deixando os arquivos originais inalterados.
+   * Converte todos os arquivos CSV da pasta de entradas. Os resultados são gerados na pasta de
+   * saídas, deixando os arquivos originais inalterados.
    *
    * @param pastaDeEntradas Pasta contendo os arquivos CSV gerados pela página web.
-   * @param pastaDeSaidas Pasta em que serão colocados os arquivos gerados no formato
-   *                      requerido pelo subsistema.
+   * @param pastaDeSaidas Pasta em que serão colocados os arquivos gerados no formato requerido pelo
+   *        subsistema.
    *
-   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou
-   *                     gravar os arquivos de saída.
+   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou gravar os
+   *         arquivos de saída.
    */
   public void converterPasta(File pastaDeEntradas, File pastaDeSaidas) throws IOException {
-    // TODO: Implementar.
+    pastaDeSaidas.mkdir();
+    if (pastaDeEntradas.isDirectory() && pastaDeEntradas.canRead()) {
+      for (File arquivoDeEntrada : pastaDeEntradas.listFiles()) {
+        ArrayList<String> conteudo = this.lerConteudoArquivo(arquivoDeEntrada);
+        String nomeArquivo = arquivoDeEntrada.getName();
+        File arquivoDeSaida = new File(pastaDeSaidas + File.separator + nomeArquivo);
+        this.escreverConteudoNoArquivo(arquivoDeSaida, conteudo);
+      }
+    }
+  }
+
+  public ArrayList<String> lerConteudoArquivo(File caminhoArquivo) throws IOException {
+    FileReader leitorArquivo = null;
+    BufferedReader bufferedLeitor = null;
+
+    leitorArquivo = new FileReader(caminhoArquivo);
+    bufferedLeitor = new BufferedReader(leitorArquivo);
+    ArrayList<String> linhasLidas = new ArrayList<String>();
+
+    String conteudoLinha = bufferedLeitor.readLine();
+
+    while (conteudoLinha != null) {
+      linhasLidas.add(conteudoLinha);
+      conteudoLinha = bufferedLeitor.readLine();
+      if (conteudoLinha != null) {
+        conteudoLinha = this.formartarLinha(conteudoLinha);
+      }
+    }
+    leitorArquivo.close();
+    bufferedLeitor.close();
+
+    return linhasLidas;
+  }
+
+  public void escreverConteudoNoArquivo(File caminhoArquivo, ArrayList<String> conteudo)
+      throws IOException {
+    FileWriter escritorArquivo = null;
+    BufferedWriter bufferedEscritor = null;
+    if (!caminhoArquivo.exists()) {
+      caminhoArquivo.createNewFile();
+    }
+
+    escritorArquivo = new FileWriter(caminhoArquivo, true);
+    bufferedEscritor = new BufferedWriter(escritorArquivo);
+    for (String linha : conteudo) {
+      bufferedEscritor.write(linha);
+      bufferedEscritor.newLine();
+      bufferedEscritor.flush();
+    }
+
+    escritorArquivo.close();
+    bufferedEscritor.close();
+  }
+
+  public String formartarLinha(String conteudo) {
+    String[] informacoes = conteudo.split(",");
+    String nomeCompleto = informacoes[0];
+    String dataNascimento = informacoes[1];
+    String email = informacoes[2];
+    String cpf = informacoes[3];
+
+    String[] data = dataNascimento.split("/");
+    String dia = data[0];
+    String mes = data[1];
+    String ano = data[2];
+    String dataFormatada = String.join("-", ano, mes, dia);
+
+    String cpfFormatado = cpf.substring(0, 3) + '.' + cpf.substring(3, 6) + '.'
+        + cpf.substring(6, 9) + '-' + cpf.substring(9);
+
+    return String.join(",", nomeCompleto.toUpperCase(), dataFormatada, email, cpfFormatado);
   }
 }
